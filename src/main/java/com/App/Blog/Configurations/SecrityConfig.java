64be7956b7ddac9ea2373b5f69146d4cdac4beb9
Sharan-0-dot <sharan482@gmail.com>
2025-08-08 11:45:45 +0530

    cors bug fix

--- src/main/java/com/App/Blog/Configurations/SecrityConfig.java
+++ src/main/java/com/App/Blog/Configurations/SecrityConfig.java
@@ -44,7 +44,7 @@ public class SecrityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://journal-app-eta-five.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://journal-app-blush-omega.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
